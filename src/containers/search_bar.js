c422eabbedd5c11cff88aa9457518501dd4bf2e7
briandridge <briandridge@gmail.com>
2017-04-24 14:08:23 -0600

    works through 54

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -10,14 +10,16 @@ export default class SearchBar extends Component {
 	}
 
 	onInputChange(event) {
-		console.log(event.target.value);
-
 		this.setState({ term: event.target.value});
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
 	}
 
 	render() {
 		return(
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input
 					placeholder="Get a forecast"
 					className="form-control"
